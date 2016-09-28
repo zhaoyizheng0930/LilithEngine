497544834beb469f7042f3aa57a137a2ea742acc
zhaoyizheng <zhaoyizheng0930@hotmail.com>
2016-09-28 17:56:44 +0800

    Memory/MallocTBB.cpp continue commit..

--- Source/Runtime/Core/Private/HAL/Memory/MallocTBB.cpp
+++ Source/Runtime/Core/Private/HAL/Memory/MallocTBB.cpp
@@ -1,11 +1,18 @@
 #include "../../../Public/HAL/Memory/MallocTBB.h"
 
-void* FMallocTBB::Malloc(SIZE_T Count, uint32 Alignment = DEFAULT_ALIGNMENT)
+void* FMallocTBB::Malloc(SIZE_T Count, uint32 Alignment)
 {
+	if (Alignment == DEFAULT_ALIGNMENT)
+	{
+		//scalable
+	}
+	else
+	{
 
+	}
 }
 
-void* FMallocTBB::Realloc(void* Original, SIZE_T Count, uint32 Alignment = DEFAULT_ALIGNMENT)
+void* FMallocTBB::Realloc(void* Original, SIZE_T Count, uint32 Alignment)
 {
 
 }
